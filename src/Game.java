e7d3d7d957471d595c0ed9ccc03beb13ad329e20
Jay <jayasurya.sridharan@gmail.com>
2015-02-12 11:14:52 -0600

    Added Game class.

---
+++ src/Game.java
@@ -0,0 +1,33 @@
+import org.lwjgl.Sys;
+import org.lwjgl.glfw.*;
+import org.lwjgl.opengl.*;
+
+import java.nio.ByteBuffer;
+ 
+import static org.lwjgl.glfw.Callbacks.*;
+import static org.lwjgl.glfw.GLFW.*;
+import static org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.system.MemoryUtil.*;
+ * 
+ */
+
+/**
+ * @author jsridha2
+ *
+ */
+public class Game {
+
+	
+	public Game(){
+	
+		render();
+		
+	}
+	
+	private void render(){
+	
+	}
+
+}
+	
+
