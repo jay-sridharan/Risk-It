7153a198f80a9ab52ed2bff985319f82f63b8e0e
Jay <jayasurya.sridharan@gmail.com>
2015-02-13 11:13:47 -0600

    Added prliminary texture code and Game class
    https://www.youtube.com/watch?v=naE3nbreSUo

--- src/Game.java
+++ src/Game.java
@@ -1,33 +1,145 @@
 import org.lwjgl.Sys;
 import org.lwjgl.glfw.*;
 import org.lwjgl.opengl.*;
+import org.newdawn.slick.opengl.Texture;
+import org.newdawn.slick.opengl.TextureLoader;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.nio.ByteBuffer;
- 
+
 import static org.lwjgl.glfw.Callbacks.*;
 import static org.lwjgl.glfw.GLFW.*;
 import static org.lwjgl.opengl.GL11.*;
 import static org.lwjgl.system.MemoryUtil.*;
- * 
- */
 
 /**
  * @author jsridha2
  *
  */
 public class Game {
+	private GLFWErrorCallback errorCallback;
+	private GLFWKeyCallback keyCallback;
+	// The window handle
+	private long window;
 
-	
-	public Game(){
-	
-		render();
+	public Game() {
+
+		System.out.println("Hello LWJGL " + Sys.getVersion() + "!");
+
+		try {
+			init();
+			loop();
+
+			// Release window and window callbacks
+			glfwDestroyWindow(window);
+			keyCallback.release();
+		} finally {
+			// Terminate GLFW and release the GLFWerrorfun
+			glfwTerminate();
+			errorCallback.release();
+		}
+	}
+
+	private void init() {
+		// Setup an error callback. The default implementation
+		// will print the error message in System.err.
+		glfwSetErrorCallback(errorCallback = errorCallbackPrint(System.err));
+
+		// Initialize GLFW. Most GLFW functions will not work before doing this.
+		if (glfwInit() != GL11.GL_TRUE) {
+			throw new IllegalStateException("Unable to initialize GLFW");
+		}
+
+		// Configure our window
+		glfwDefaultWindowHints(); // optional, the current window hints are
+									// already the default
+		glfwWindowHint(GLFW_VISIBLE, GL_FALSE); // the window will stay hidden
+												// after creation
+		glfwWindowHint(GLFW_RESIZABLE, GL_TRUE); // the window will be resizable
+
 		
-	}
+		// Get the resolution of the primary monitor
+		ByteBuffer vidmode = glfwGetVideoMode(glfwGetPrimaryMonitor());
+		int WIDTH = (int) Math.floor((GLFWvidmode.width(vidmode)) * 0.75);
+		int HEIGHT = (int) Math.floor((GLFWvidmode.height(vidmode)) * 0.75);
+
+		// Create the window
+		window = glfwCreateWindow(WIDTH, HEIGHT, "Risk It!", NULL, NULL);
+		if (window == NULL)
+			throw new RuntimeException("Failed to create the GLFW window");
+
+		// Setup a key callback. It will be called every time a key is pressed,
+		// repeated or released.
+		glfwSetKeyCallback(window, keyCallback = new GLFWKeyCallback() {
+			@Override
+			public void invoke(long window, int key, int scancode, int action,
+					int mods) {
+				if (key == GLFW_KEY_ESCAPE && action == GLFW_RELEASE)
+					glfwSetWindowShouldClose(window, GL_TRUE); // We will detect
+																// this in our
+																// rendering
+																// loop
+			}
+		});
+
 	
-	private void render(){
-	
+		// Center our window
+		glfwSetWindowPos(window, (GLFWvidmode.width(vidmode) - WIDTH) / 2,
+				(GLFWvidmode.height(vidmode) - HEIGHT) / 2);
+
+		// Make the OpenGL context current
+		glfwMakeContextCurrent(window);
+		// Enable v-sync
+		glfwSwapInterval(1);
+
+		// Make the window visible
+		glfwShowWindow(window);
 	}
 
+	private void loop() {
+		// This line is critical for LWJGL's interoperation with GLFW's
+		// OpenGL context, or any context that is managed externally.
+		// LWJGL detects the context that is current in the current thread,
+		// creates the ContextCapabilities instance and makes the OpenGL
+		// bindings available for use.
+		GLContext.createFromCurrent();
+
+		float color = (float) 0.2431372549019608;
+		Texture texture = loadTexture("menuTexture","png");
+		
+		// Set the clear color
+		glClearColor(color, color, color, 1.0f);
+
+		// Run the rendering loop until the user has attempted to close
+		// the window or has pressed the ESCAPE key.
+		while (glfwWindowShouldClose(window) == GL_FALSE) {
+			glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // clear the
+																// framebuffer
+
+			glfwSwapBuffers(window); // swap the color buffers
+
+			// Poll for window events. The key callback above will only be
+			// invoked during this call.
+			glfwPollEvents();
+		}
+
+	}
+
+	private Texture loadTexture(String id, String type){
+		try {
+			return TextureLoader.getTexture("PNG",new FileInputStream(new File("util/img/menu" + id + "." + type)));
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
+
+	}
+	
 }
-	
-
