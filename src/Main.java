1488a9c1cae4355336b223116bf074f545888454
Jay Sridharan <jayasurya.sridharan@gmail.com>
2015-02-24 20:20:06 -0600

    Added neccessary files.

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,11 @@
-
 public class Main {
 
+	@SuppressWarnings("unused")
 	public Main() {
 		Game game = new Game();
 	}
 
+	@SuppressWarnings("unused")
 	public static void main(String[] args) {
 		Main main = new Main();
 	}
